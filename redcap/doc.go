395be39f799bcdbb35c62e03d12724f5dd0db9a1
Tyler Rivera <riverat2@email.chop.edu>
2015-07-28 14:02:05 -0400

    Formatting

--- redcap/doc.go
+++ redcap/doc.go
@@ -1,18 +1,18 @@
 /*
-    Package redcap provides a REDCap client implementation focusing
-    around the concept of a REDCap project.
+   Package redcap provides a REDCap client implementation focusing
+   around the concept of a REDCap project.
 
-    Most users will interact with REDCap in the context of a project:
+   Most users will interact with REDCap in the context of a project:
 
-            project := redcap.NewRedcapProject("http://redcap.myinstitution.org", "<redcap-token>", true)
+           project := redcap.NewRedcapProject("http://redcap.myinstitution.org", "<redcap-token>", true)
 
-    Retrieve a prject's metadata and forms:
+   Retrieve a prject's metadata and forms:
 
-            fields := project.GetMetadata()
-            ...
-            forms := project.GetForms()
-            ...
+           fields := project.GetMetadata()
+           ...
+           forms := project.GetForms()
+           ...
 
-    
+
 */
 package redcap
